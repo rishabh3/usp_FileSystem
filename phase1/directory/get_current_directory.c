fef906e815d06ccf114609b2b62af98495ea32da
Rishabh Dutt <rishabh97dutt@gmail.com>
2018-01-26 13:40:53 +0530

    Some more implementations

---
+++ phase1/directory/get_current_directory.c
@@ -0,0 +1,13 @@
+/* USP PROJECT --- Phase1 *
+ * Ravi Shreyas 	  *
+ * Rishabh Dutt 	  *
+ * Rajat Nigam		  */
+
+char * get_current_directory(){
+	char* pathname = NULL;
+	// get the directory structure
+	// start with .
+	// follow .. and append the name to path
+	// 
+	return pathname;
+}
